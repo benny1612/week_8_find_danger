f2f30fb09763b55ab6b3a95665137705e2d4481b
benny_neiman <bn161299@gmail.com>
2025-12-17 12:23:42 +0200

    menu

--- func.js
+++ func.js
@@ -1,54 +1,80 @@
-import fs from "fs/promises"
+import fs from "fs/promises";
 
-
-export async function GetPeopleList () {
+export async function GetPeopleList() {
   const res = await fetch(`https://spiestestserver-8l55.onrender.com/people`);
   try {
-    const data = await res.text()
-    fs.writeFile('PEOPLE.json', data, 'utf8', (err) => {
-  if (err) {
-    console.error('Error writing file:', err);
-    return;
-  }
-  console.log('File written successfully!');
-});
-    
-    const js_data=JSON.parse(data)
-    return js_data
+    const data = await res.text();
+    fs.writeFile("PEOPLE.json", data, "utf8", (err) => {
+      if (err) {
+        console.error("Error writing file:", err);
+        return;
+      }
+      console.log("File written successfully!");
+    });
+
+    const js_data = JSON.parse(data);
+    return js_data;
   } catch (error) {
     console.error("Failed to load data");
   }
 }
 
-
-export async function GetCallRecordst () {
-  const res = await fetch(`https://spiestestserver-8l55.onrender.com/transcriptions`);
+export async function GetCallRecordst() {
+  const res = await fetch(
+    `https://spiestestserver-8l55.onrender.com/transcriptions`
+  );
   try {
-    const data = await res.text()
-    fs.writeFile('TRANSCRIPTIONS.json', data, 'utf8', (err) => {
-  if (err) {
-    console.error('Error writing file:', err);
-    return;
-  }
-  console.log('File written successfully!');
-});
-    
-    const js_data=JSON.parse(data)
-    return js_data
+    const data = await res.text();
+    fs.writeFile("TRANSCRIPTIONS.json", data, "utf8", (err) => {
+      if (err) {
+        console.error("Error writing file:", err);
+        return;
+      }
+      console.log("File written successfully!");
+    });
+
+    const js_data = JSON.parse(data);
+    return js_data;
   } catch (error) {
     console.error("Failed to load data");
   }
 }
 
-export async function SearchPeoplebyName(name){
-try {
-    const file = await fs.readFile('./PEOPLE.json', 'utf8')
-} catch (error) {console.error(error)
-    
-}
-const js_file =JSON.parse(file)
-js_file.forEach(element => {if (element.name==name)
-    console.log(element)
-});
+export async function SearchPeoplebyName(name) {
+  const file = await fs.readFile("./PEOPLE.json", "utf8");
 
-}
+  const js_file = JSON.parse(file);
+  const result =js_file.filter((person) => person.name==name)
+  if (result.length >0){
+    console.log (result)}else{console.log("person not found")}
+  }
+
+
+
+export async function SearchPeoplebyAge(age) {
+  const file = await fs.readFile("./PEOPLE.json", "utf8");
+
+  const js_file = JSON.parse(file);
+ const result =js_file.filter((person) => person.age==age)
+  if (result.length >0){
+    console.log (result)}else{console.log("person not found")}
+  }
+
+
+export async function FindDangerousPeople() {
+  const file = await fs.readFile("./TRANSCRIPTIONS.json", "utf8");
+  const js_file = JSON.parse(file);
+  const dangers_calls=js_file.filter((element)=>
+      element.content.toLowerCase().includes("death") ||
+      element.content.toLowerCase().includes("knife") ||
+      element.content.toLowerCase().includes("bomb") ||
+      element.content.toLowerCase().includes("attack")
+    )
+     
+console.log(dangers_calls)
+
+    
+  }
+// await GetCallRecordst();
+// await FindDangerousPeople();
+// export const get_p= await GetPeopleList()
---
+++ menu.js
@@ -0,0 +1,38 @@
+import input from 'analiza-sync';
+import {FindDangerousPeople, GetCallRecordst, GetPeopleList, SearchPeoplebyAge, SearchPeoplebyName } from './func.js';
+import { Console } from 'console';
+ async function menu(){
+    const flag =true
+    while(flag){
+         let message= input("\nWhat would you like to do?\n1.Get People List:\n2.Get Call Records/Transcriptions:\n3.Search People by Name:\n4.Search People by Age: \n5.Find Dangerous People \n6.exit:\n>")
+         switch(message){
+            case "1":
+                console.log (await GetPeopleList())
+                break;
+            case "2":
+                console.log(await GetCallRecordst())
+                break;
+            case "3":
+               let name=input("enter a name:\n>")
+               await SearchPeoplebyName(name)
+               break;
+            case "4":
+                let age =input("enter a age :\n>")
+                await SearchPeoplebyAge(age)
+                break;
+            case "5":
+                console.log("כרגע רק מחזיר את האובייקטים שמכילים את המילים ")
+                await FindDangerousPeople()
+                break;
+            case "6":
+                console.log("goodbye!")
+                flag =false
+                break;
+
+
+
+
+         }
+    }
+}
+menu()
